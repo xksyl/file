7ca4a453a4914489109d60bc84d675c4789d2519
xksyl <egorka2004212@gmail.com>
2024-07-18 14:53:31 +0500

    Add files via upload

---
+++ new file.py
@@ -0,0 +1,15 @@
+def test_function():
+
+    def inner_function():
+        text = "Я в области видимости функции test_function"
+        print(text)
+
+    inner_function()
+    
+test_function()
+#inner_function()
+
+
+
+
+
